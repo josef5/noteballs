d014a39dd91352640a779f0f17d63dbbce0772b5
Jose Espejo <joseespejo@yahoo.com>
2024-03-14 18:25:09 +0000

    refactor: use addDoc instead of setDoc
    This enables firebase auto id

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -3,41 +3,45 @@ import {
   collection,
   onSnapshot,
   doc,
-  setDoc,
   deleteDoc,
   updateDoc,
   query,
-  orderBy
+  orderBy,
+  addDoc
 } from 'firebase/firestore'
 import { db } from '@/db/firebase'
 
 const notesCollectionRef = collection(db, 'notes')
-const notesCollectionQuery = query(notesCollectionRef, orderBy('id', 'desc'))
+const notesCollectionQuery = query(notesCollectionRef, orderBy('date', 'desc'))
 
 export const useStoreNotes = defineStore('storeNotes', {
   state: () => ({
-    notes: []
+    notes: [],
+    notesLoaded: false
   }),
   actions: {
     async getNotes() {
+      this.notesLoaded = false
+
       onSnapshot(notesCollectionQuery, (querySnapshot) => {
         let notes = []
 
         querySnapshot.forEach((doc) => {
           // doc.data() is never undefined for query doc snapshots
-          let note = { id: doc.id, content: doc.data().content }
+          let note = { id: doc.id, content: doc.data().content, date: doc.data().date }
 
           notes.push(note)
         })
 
         this.notes = notes
+        this.notesLoaded = true
       })
     },
     async addNote(content) {
-      const id = new Date().getTime().toString()
+      const date = new Date().getTime().toString()
 
-      await setDoc(doc(notesCollectionRef, id), {
-        id,
+      await addDoc(notesCollectionRef, {
+        date,
         content
       })
     },
@@ -51,8 +55,7 @@ export const useStoreNotes = defineStore('storeNotes', {
     }
   },
   getters: {
-    getNoteContent: (state) => (id) =>
-      state.notes.find((note) => note.id === id.toString()).content,
+    getNoteContent: (state) => (id) => state.notes.find((note) => note.id === id).content,
     getNoteById: (state) => (id) => {
       return state.notes.find((note) => note.id === id)
     },
