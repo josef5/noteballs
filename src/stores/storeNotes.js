0244a4e0c1aa7b222f89550208deded1385008dc
Jose Espejo <joseespejo@yahoo.com>
2024-03-18 15:52:49 +0000

    refactor: notesCollection var name
    From notesCollectionRef

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -12,7 +12,7 @@ import {
 import { db } from '@/db/firebase'
 import { useStoreAuth } from '@/stores/storeAuth'
 
-let notesCollectionRef
+let notesCollection
 let notesCollectionQuery
 let unsubscribeNotesSnapshot = null
 
@@ -25,8 +25,8 @@ export const useStoreNotes = defineStore('storeNotes', {
     init() {
       const storeAuth = useStoreAuth()
 
-      notesCollectionRef = collection(db, 'users', storeAuth.user.id, 'notes')
-      notesCollectionQuery = query(notesCollectionRef, orderBy('date', 'desc'))
+      notesCollection = collection(db, 'users', storeAuth.user.id, 'notes')
+      notesCollectionQuery = query(notesCollection, orderBy('date', 'desc'))
 
       this.getNotes()
     },
@@ -37,35 +37,41 @@ export const useStoreNotes = defineStore('storeNotes', {
         unsubscribeNotesSnapshot()
       }
 
-      unsubscribeNotesSnapshot = onSnapshot(notesCollectionQuery, (querySnapshot) => {
-        let notes = []
+      unsubscribeNotesSnapshot = onSnapshot(
+        notesCollectionQuery,
+        (querySnapshot) => {
+          let notes = []
 
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          let note = { id: doc.id, content: doc.data().content, date: doc.data().date }
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            let note = { id: doc.id, content: doc.data().content, date: doc.data().date }
 
-          notes.push(note)
-        })
+            notes.push(note)
+          })
 
-        this.notes = notes
-        this.notesLoaded = true
-      })
+          this.notes = notes
+          this.notesLoaded = true
+        },
+        (error) => {
+          console.error(error)
+        }
+      )
     },
     async addNote(content) {
       const date = new Date().getTime().toString()
 
-      await addDoc(notesCollectionRef, {
+      await addDoc(notesCollection, {
         date,
         content
       })
     },
     async updateNote(id, content) {
-      await updateDoc(doc(notesCollectionRef, id.toString()), {
+      await updateDoc(doc(notesCollection, id.toString()), {
         content
       })
     },
     async deleteNote(id) {
-      await deleteDoc(doc(notesCollectionRef, id))
+      await deleteDoc(doc(notesCollection, id))
     },
     clearNotes() {
       this.notes = []
